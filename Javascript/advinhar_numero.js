a1f19d2d3722ea189bcab43d33b384b72c512e23
ingrain576 <32407690+ingrain576@users.noreply.github.com>
2020-11-05 14:02:02 -0300

    update

--- Javascript/advinhar_numero.js
+++ Javascript/advinhar_numero.js
@@ -1,8 +1,10 @@
 let numUsuario = document.querySelector('#num-chute'),
     tentarBtn = document.querySelector('#tentar-btn'),
     msg = document.querySelector('#mensagem')
+    chancesTxt = document.querySelector('#chances')
+    fundo = document.querySelector('#container')
 
-let numChances = 3,
+let numChances = 5,
     numSorteado = Math.floor((Math.random() * 10) + 1)
 
 tentarBtn.addEventListener('click', function(e){
@@ -13,9 +15,21 @@ tentarBtn.addEventListener('click', function(e){
     if(parseInt(numUsuario.value) === numSorteado) {
         alert('Você acertou o número, jogar novamente?')
         window.location.reload()
+    } else if (Math.abs(numSorteado - numUsuario.value) <= 2) {
+        numChances <= 1 ? gameOver() : numChances -= 1
+        msg.textContent = `Número ${numUsuario.value} está quente.`
+        fundo.style.backgroundColor = 'rgb(255, 51, 0)'
+        chancesTxt.textContent = `Chances: ${numChances}`
+    } else if (Math.abs(numSorteado - numUsuario.value) >= 3 && Math.abs(numSorteado - numUsuario.value) <= 4) {
+        numChances <= 1 ? gameOver() : numChances -= 1
+        msg.textContent = `Número ${numUsuario.value} está morno.`
+        fundo.style.backgroundColor = '#e6e600'
+        chancesTxt.textContent = `Chances: ${numChances}`
     } else {
         numChances <= 1 ? gameOver() : numChances -= 1
-        msg.textContent = `Você errou! Chances restantes: ${numChances}`
+        msg.textContent = `Número ${numUsuario.value} está frio.`
+        chancesTxt.textContent = `Chances: ${numChances}`
+        fundo.style.backgroundColor = 'rgb(51, 204, 255)'
     }
     numUsuario.value = ''
 })
