97356ea52c51b63920d23591f98ec1d41bba46a9
ingrain576 <32407690+ingrain576@users.noreply.github.com>
2020-11-05 14:08:09 -0300

    Update advinhar_numero.js

--- Javascript/advinhar_numero.js
+++ Javascript/advinhar_numero.js
@@ -2,8 +2,7 @@ let numUsuario = document.querySelector('#num-chute'),
     tentarBtn = document.querySelector('#tentar-btn'),
     msg = document.querySelector('#mensagem')
     chancesTxt = document.querySelector('#chances')
-    fundo = document.querySelector('#container')
-
+    fundo = document.body
 let numChances = 5,
     numSorteado = Math.floor((Math.random() * 10) + 1)
 
@@ -15,12 +14,12 @@ tentarBtn.addEventListener('click', function(e){
     if(parseInt(numUsuario.value) === numSorteado) {
         alert('Você acertou o número, jogar novamente?')
         window.location.reload()
-    } else if (Math.abs(numSorteado - numUsuario.value) <= 2) {
+    } else if (Math.abs(numSorteado - numUsuario.value) == 1) {
         numChances <= 1 ? gameOver() : numChances -= 1
         msg.textContent = `Número ${numUsuario.value} está quente.`
         fundo.style.backgroundColor = 'rgb(255, 51, 0)'
         chancesTxt.textContent = `Chances: ${numChances}`
-    } else if (Math.abs(numSorteado - numUsuario.value) >= 3 && Math.abs(numSorteado - numUsuario.value) <= 4) {
+    } else if (Math.abs(numSorteado - numUsuario.value) <= 3) {
         numChances <= 1 ? gameOver() : numChances -= 1
         msg.textContent = `Número ${numUsuario.value} está morno.`
         fundo.style.backgroundColor = '#e6e600'
