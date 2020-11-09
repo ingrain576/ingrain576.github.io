224ade5346cdf4aa0e46ee054b4e803aa0ab9421
ingrain576 <32407690+ingrain576@users.noreply.github.com>
2020-11-09 13:05:02 -0300

    site com flexbox

---
+++ VueJS/desafio-01/app.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        nome: 'Filipe',
+        idade: '28',
+        link: 'https://f.i.uol.com.br/fotografia/2018/07/16/15317548545b4cb96655a4b_1531754854_3x2_lg.jpg'
+    },
+    methods: {
+        numRandom() {
+            return Math.random()
+        }
+    }
+})
+
